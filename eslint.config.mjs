0849fd4199d65880e0f249f604df8fed091c6a8e
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-05-29 01:25:17 -0700

    chore: Add tags to project configurations and enforce dependency rules in ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,31 @@ export default [
               sourceTag: '*',
               onlyDependOnLibsWithTags: ['*'],
             },
+            // Enforce that browser runtime projects do not depend on node runtime projects
+            {
+              sourceTag: 'runtime:browser',
+              notDependOnLibsWithTags: ['runtime:node'],
+            },
+            // Enforce that node runtime projects do not depend on browser runtime projects
+            {
+              sourceTag: 'runtime:node',
+              notDependOnLibsWithTags: ['runtime:browser'],
+            },
+            // Customer apps should not depend on admin apps
+            {
+              sourceTag: 'scope:customer',
+              notDependOnLibsWithTags: ['scope:admin'],
+            },
+            // Admin apps should not depend on customer apps
+            {
+              sourceTag: 'scope:admin',
+              notDependOnLibsWithTags: ['scope:customer'],
+            },
+            // Constracts cannot import from any other project except other contracts
+            {
+              sourceTag: 'type:contract',
+              onlyDependOnLibsWithTags: ['type:contract'],
+            },
           ],
         },
       ],
