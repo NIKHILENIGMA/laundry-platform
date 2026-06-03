b7d00b5bd6da027d72d153d9006d33ecdb9a2a48
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-06-03 00:58:48 -0700

    chore(repo): setup git hooks and code quality checks

---
+++ commitlint.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'web',
+        'admin-web',
+        'api',
+        'api-e2e',
+        'ui',
+        'repo',
+        'deps',
+        'config',
+      ],
+    ],
+  },
+}
